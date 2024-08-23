abf26c8c64ebc5698d54df7735ad31bd738b5ab0
Omran950 <mohammedomran32@gmail.com>
2024-08-24 02:41:51 +0300

    initial commit

---
+++ src/pages/SeriesPage.jsx
@@ -0,0 +1,28 @@
+import { Grid } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Card } from "../Components/Card";
+import { fetchSeries } from "../Controllers/MovieController";
+
+export const SeriesPage = () => {
+  const [series, setSeries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchSeries();
+      setSeries(data);
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Grid container spacing={2} sx={{ padding: "20px" }}>
+      {series.map((serie) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={serie.id}>
+          <Card type={"series"} item={serie} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
---
+++ src/pages/TVShowsDetails.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchTVShowDetails } from "../Controllers/MovieController";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+  Container,
+} from "@mui/material";
+
+const TVShowsDetails = () => {
+  const { id } = useParams();
+  const [details, setDetails] = useState({});
+  useEffect(() => {
+    const getDetails = async () => {
+      const data = await fetchTVShowDetails(id);
+      setDetails(data);
+      console.log(data);
+    };
+
+    getDetails();
+  }, [id]);
+
+  return (
+    <Container>
+      <Card>
+        <CardMedia
+          component="img"
+          height={500}
+          image={`https://image.tmdb.org/t/p/w500/${details.poster_path}`}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Name : {details.name}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            rating: {details.vote_average}
+          </Typography>
+
+          <Typography>
+            Overview : <br /> {details.overview}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
+
+export default TVShowsDetails;
